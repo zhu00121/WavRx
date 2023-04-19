9e4625c4cc45d7e64c0e0222d089af267a4d8838
yizhu <Yi.Zhu@inrs.ca>
2023-04-19 15:48:44 -0700

    minor edits

---
+++ exps/one-vs-all/prepare_diagnostics_data.py
@@ -0,0 +1,167 @@
+"""
+TODO: add description
+"""
+
+import os, glob
+import random
+import pandas as pd
+import json
+from path import Path
+import shutil
+import logging
+import torchaudio
+
+logger = logging.getLogger(__name__)
+
+
+def prepare_data(
+    wav_folder:str,
+    audio_archive_path:str,
+    metadata_path:str,
+    manifest_train_path:str,
+    manifest_valid_path:str,
+    manifest_test_path:str,
+    ratio:list = [0.7,0.2,0.1],
+    random_seed:int = 2023
+    ):
+
+    """
+    Input:
+    ---
+    wav_folder: path to the folder where all audio files are stored. Default as './data_og/Cambridge/wav/EN'
+    metadata_path: path to the metadata file (.csv). Default as './data_og/Cambridge/metadata/EN-metadata.csv'
+    manifest_path: path to the manifest file (.json).
+    """
+
+    # Check if this phase is already done (if so, skip it)
+    if skip(manifest_train_path, manifest_valid_path, manifest_test_path):
+        logger.info("Manifest files preparation completed in previous run, skipping.")
+        return
+    
+    if not os.path.exists(metadata_path):
+        raise ValueError("Metadata file not found. Check the metadata folder. ")
+
+    # If the wav folder does not exist, unzip the audio zip file
+    if not check_folders(wav_folder): 
+        unzip_audio_file(wav_folder, audio_archive_path)
+    else: print('Found extracted audio files. Skip unzipping.')
+
+    # List files and create manifest from list
+    logger.info(
+        f"Creating {manifest_train_path}, {manifest_valid_path}, and {manifest_test_path}"
+    )
+    
+    # Creating json files for train, valid, and test all at once
+    create_json(wav_folder, 
+                metadata_path, 
+                [manifest_train_path,manifest_valid_path,manifest_test_path],
+                ratio,
+                random_seed)
+
+
+def create_json(wav_folder:str, metadata_path:str, manifest_paths:list, ratio:list, random_seed:int):
+    """
+    Creates the manifest file given the metadata file.
+    """
+
+    # Load metadata file
+    df_metadata = pd.read_csv(metadata_path, sep=';')
+    # Calculate total number of audio files
+    wav_files = glob.glob(os.path.join(wav_folder, "*.wav"), recursive=True)
+    print("Total wav audio files {} in the audio folder".format(len(wav_files)))
+    # Sanity check if number of files in metadata is in consistency with number of files in the audio folder
+    assert len(wav_files) == df_metadata.shape[0], "Number of audio files wein the folder is not consistent with number of samples in the metadata"
+
+    # Split the metadata file into train,valid,and test files
+    df_train, df_valid, df_test = split_metadata(df_metadata, ratio, random_seed)
+    dataframe_to_json(df_train,manifest_paths[0])
+    dataframe_to_json(df_valid,manifest_paths[1])
+    dataframe_to_json(df_test,manifest_paths[2])
+
+    logger.info(f"{manifest_paths} successfully created!")
+
+
+def split_metadata(metadata_df, ratio, random_seed):
+    """
+    User-independent split with each split contains similar class ratio
+    """
+
+    mask_1 = metadata_df['Cough-label'] == 'cough'
+    mask_2 = metadata_df['Cough-label'] == 'non'
+    uid_pos = list(metadata_df[mask_1]['Uid'].unique())
+    uid_neg = list(metadata_df[mask_2]['Uid'].unique())
+    uid_pos = list(set(uid_pos).difference(set(uid_neg)))
+    
+    random.Random(random_seed).shuffle(uid_pos)
+    random.Random(random_seed).shuffle(uid_neg)
+
+    train_spk_pos = uid_pos[:int(len(uid_pos)*ratio[0])]
+    train_spk_neg = uid_neg[:int(len(uid_neg)**ratio[0])]
+    valid_spk_pos = uid_pos[int(len(uid_pos)**ratio[0]):int(len(uid_pos)*ratio[1])]
+    valid_spk_neg = uid_neg[int(len(uid_neg)**ratio[0]):int(len(uid_pos)*ratio[1])]
+    test_spk_pos = uid_pos[int(len(uid_pos)*ratio[1]):]
+    test_spk_neg = uid_neg[int(len(uid_neg)*ratio[1]):]
+
+    train_spk = train_spk_pos + train_spk_neg
+    valid_spk = valid_spk_pos + valid_spk_neg
+    test_spk = test_spk_pos + test_spk_neg
+
+    train_df = metadata_df[metadata_df['Uid'].isin(train_spk)]
+    valid_df = metadata_df[metadata_df['Uid'].isin(valid_spk)]
+    test_df = metadata_df[metadata_df['Uid'].isin(test_spk)]
+
+    return train_df, valid_df, test_df
+
+
+def dataframe_to_json(df,save_path):
+    # we now build JSON examples 
+    examples = {}
+    for _, row in df.iterrows():
+        utt_id = Path(row['voice-path-new']).stem # returns the name (without extension) E.g., '00000','00010'
+        examples[utt_id] = {"ID": utt_id,
+                            "file_path": row['voice-path-new'], 
+                            "cough-label": row['Cough-label'],
+                            "symptom": row['Symptoms'],
+                            "length": torchaudio.info(row['voice-path-new']).num_frames}
+        
+    if not os.path.exists(os.path.dirname(save_path)):
+        os.makedirs(os.path.dirname(save_path))
+
+    with open(save_path, "w") as f:
+        json.dump(examples, f, indent=4)
+
+    return examples
+
+
+def skip(*filenames):
+    """
+    Detects if the data preparation has been already done.
+    If the preparation has been done, we can skip it.
+
+    Returns
+    -------
+    bool
+        if True, the preparation phase can be skipped.
+        if False, it must be done.
+    """
+    for filename in filenames:
+        if not os.path.isfile(filename):
+            return False
+    return True
+
+
+def check_folders(*folders):
+    """Returns False if any passed folder does not exist."""
+    for folder in folders:
+        if not os.path.exists(folder):
+            return False
+    return True
+
+
+def unzip_audio_file(destination, audio_archive_path):
+    """
+    Unzip the compressed audio folder.
+    """
+    if not os.path.exists(audio_archive_path):
+        raise ValueError("Audio zip file not found. Please refer to prep.ipynb first to prepare the zip file.")
+    shutil.unpack_archive(os.path.dirname(audio_archive_path), destination) # this will create a folder called 'EN' inside of the 'wav' folder
---
+++ exps/one-vs-all/train.py
@@ -0,0 +1,309 @@
+#!/usr/bin/env python3
+"""Recipe for training a symptom diagnostics model.
+
+To run this recipe, do the following:
+> python train.py manifest/train.yaml
+
+To read the code, first scroll to the bottom to see the "main" code.
+This gives a high-level overview of what is going on, while the
+Brain class definition provides the details of what happens
+for each batch during training.
+
+Noise and reverberation are automatically added to each sample from OpenRIR.
+
+Authors
+ * Yi Zhu 2023
+"""
+import os
+import sys
+sys.path.append('./model')
+import torch
+import torchaudio
+import torchaudio.functional as F
+import speechbrain as sb
+from hyperpyyaml import load_hyperpyyaml
+from prepare_diagnostics_data import prepare_data
+
+
+# Brain class for speech enhancement training
+class DiagnosticsBrain(sb.Brain):
+    """Class that manages the training loop."""
+
+    def compute_forward(self, batch, stage):
+        """Runs all the computation of that transforms the input into the
+        output probabilities over the N classes.
+        """
+        batch = batch.to(self.device)
+        wavs, _ = self.augment_input(batch.signal, stage)
+        # wavs = wavs.squeeze()
+        predictions = self.modules.model(wavs)
+        return predictions
+    
+    def augment_input(self, wavs, stage):
+        wavs, lens = wavs
+        # Add augmentation if specified. In this version of augmentation, we
+        # concatenate the original and the augment batches in a single bigger batch.
+        if stage == sb.Stage.TRAIN:
+            if hasattr(self.hparams, "env_corrupt"):
+                wavs_noise = self.hparams.env_corrupt(wavs, lens)
+                wavs = torch.cat([wavs, wavs_noise], dim=0)
+                lens = torch.cat([lens, lens])
+
+            if hasattr(self.hparams, "augmentation"):
+                wavs = self.hparams.augmentation(wavs, lens)
+
+        return wavs, lens
+
+
+    def compute_objectives(self, predictions, batch, stage):
+        """Computes the loss given the predicted and targeted outputs.
+
+        Arguments
+        ---------
+        predictions : tensor
+            The output tensor from `compute_forward`.
+        batch : PaddedBatch
+            This batch object contains all the relevant tensors for computation.
+        stage : sb.Stage
+            One of sb.Stage.TRAIN, sb.Stage.VALID, or sb.Stage.TEST.
+
+        Returns
+        -------
+        loss : torch.Tensor
+            A one-element tensor used for backpropagating the gradient.
+        """
+        _, lens = batch.signal
+        lab, _ = batch.cough_label_encoded
+        lab = lab.to(self.device)
+
+        # Concatenate labels (due to data augmentation)
+        if stage == sb.Stage.TRAIN and hasattr(self.hparams, "env_corrupt"):
+            lab = torch.cat([lab, lab], dim=0)
+            lens = torch.cat([lens, lens])
+
+        # Compute the cost function: BCE for a fake/genuine classification problem
+        weight = torch.tensor([1.5]).to(self.device)
+        loss = sb.nnet.losses.bce_loss(predictions, lab, pos_weight=weight)
+
+        # Append this batch of losses to the loss metric for easy
+        self.loss_metric.append(
+            batch.id, predictions, lab, lens, reduction="batch"
+        )
+
+        # Compute classification error at test time
+        if stage != sb.Stage.TRAIN:
+            self.error_metrics.append(batch.id, predictions, lab)
+
+        return loss
+
+    def on_stage_start(self, stage, epoch=None):
+        """Gets called at the beginning of each epoch.
+
+        Arguments
+        ---------
+        stage : sb.Stage
+            One of sb.Stage.TRAIN, sb.Stage.VALID, or sb.Stage.TEST.
+        epoch : int
+            The currently-starting epoch. This is passed
+            `None` during the test stage.
+        """
+
+        # Set up statistics trackers for this stage
+        self.loss_metric = sb.utils.metric_stats.MetricStats(
+            metric=sb.nnet.losses.bce_loss
+        )
+
+        # Set up evaluation-only statistics trackers
+        if stage != sb.Stage.TRAIN:
+            self.error_metrics = self.hparams.error_stats()
+
+    def on_stage_end(self, stage, stage_loss, epoch=None):
+        """Gets called at the end of an epoch.
+
+        Arguments
+        ---------
+        stage : sb.Stage
+            One of sb.Stage.TRAIN, sb.Stage.VALID, sb.Stage.TEST
+        stage_loss : float
+            The average loss for all of the data processed in this stage.
+        epoch : int
+            The currently-starting epoch. This is passed
+            `None` during the test stage.
+        """
+
+        # Store the train loss until the validation stage.
+        if stage == sb.Stage.TRAIN:
+            self.train_loss = stage_loss
+
+        # Summarize the statistics from the stage for record-keeping.
+        else:
+            metrics = self.error_metrics.summarize() 
+            stats = {
+                "loss": stage_loss,
+                "precision": metrics["precision"],
+                "recall": metrics["recall"],
+                "F1": metrics["F-score"],
+            }
+
+        # At the end of validation...
+        if stage == sb.Stage.VALID:
+
+            old_lr, new_lr = self.hparams.lr_annealing(epoch)
+            sb.nnet.schedulers.update_learning_rate(self.optimizer, new_lr)
+
+            # The train_logger writes a summary to stdout and to the logfile.
+            self.hparams.train_logger.log_stats(
+                {"Epoch": epoch, "lr": old_lr},
+                train_stats={"loss": self.train_loss},
+                valid_stats=stats,
+            )
+
+            # Save the current checkpoint and delete previous checkpoints, based on F1
+            self.checkpointer.save_and_keep_only(meta=stats, max_keys=["F1"])
+
+        # We also write statistics about test data to stdout and to the logfile.
+        if stage == sb.Stage.TEST:
+            self.hparams.train_logger.log_stats(
+                {"Epoch loaded": self.hparams.epoch_counter.current},
+                test_stats=stats,
+            )
+
+
+def dataio_prep(hparams):
+    """This function prepares the datasets to be used in the brain class.
+    It also defines the data processing pipeline through user-defined functions.
+    We expect `prepare_deepfake_trackXX` to have been called before this,
+    so that the `train.csv`, `valid.csv` manifest files are available.
+    """
+
+    # Initialization of the label encoder. The label encoder assigns to each
+    # of the observed label a unique index (e.g, 'fake': 0, 'genuine': 1, ..)
+    label_encoder = sb.dataio.encoder.CategoricalEncoder()
+
+    # Define audio pipeline
+    @sb.utils.data_pipeline.takes("file_path")
+    @sb.utils.data_pipeline.provides("signal","duration")
+    def audio_pipeline(file_path):
+        """Load the signal, and pass it and its length to the corruption class.
+        This is done on the CPU in the `collate_fn`."""
+        
+        signal, sr_og = torchaudio.load(file_path)
+        # handle multi-channel
+        if signal.shape[0] > 1:
+            signal = torch.mean(signal, axis=0)
+
+        if sr_og != 16000:
+            signal = F.resample(signal,sr_og,new_freq=16000,
+                                lowpass_filter_width=64,
+                                rolloff=0.9475937167399596,
+                                resampling_method="sinc_interp_kaiser",
+                                beta=14.769656459379492
+                                )
+        signal  = signal.squeeze()
+        duration = len(signal)
+        return signal, duration
+
+    # Define label pipeline:
+    @sb.utils.data_pipeline.takes("cough-label")
+    @sb.utils.data_pipeline.provides("cough_label_encoded")
+    def label_pipeline(label):
+        """Defines the pipeline to process the input label."""
+        label_encoder.lab2ind = {'non':0, 'cough':1}
+        label_encoded = label_encoder.encode_label_torch(label)
+        yield label_encoded
+
+    # Define datasets. We also connect the dataset with the data processing
+    # functions defined above.
+    datasets = {}
+    data_info = {
+        "train": hparams["train_annotation"],
+        "valid": hparams["valid_annotation"],
+        "test": hparams["test_annotation"]
+    }
+
+    hparams["dataloader_options"]["shuffle"] = True
+
+    for dataset in data_info:
+        datasets[dataset] = sb.dataio.dataset.DynamicItemDataset.from_json(
+            json_path=data_info[dataset],
+            # replacements={"data_root": hparams["data_folder"]},
+            dynamic_items=[audio_pipeline, label_pipeline],
+            output_keys=["id", "signal", "duration", "file_path", "cough_label_encoded", "symptom"],
+        )
+
+    # # Load or compute the label encoder (with multi-GPU DDP support)
+    # # Please, take a look into the lab_enc_file to see the label to index
+    # # mapping.
+    # lab_enc_file = os.path.join(hparams["save_folder"], "label_encoder.txt")
+    # label_encoder.load_or_create(
+    #     path=lab_enc_file,
+    #     from_didatasets=[datasets["train"]],
+    #     output_key="symptom",
+    # )
+
+    return datasets
+
+
+# Recipe begins!
+if __name__ == "__main__":
+
+    # Reading command line arguments.
+    hparams_file, run_opts, overrides = sb.parse_arguments(sys.argv[1:])
+
+    # Initialize ddp (useful only for multi-GPU DDP training).
+    sb.utils.distributed.ddp_init_group(run_opts)
+
+    # Load hyperparameters file with command-line overrides.
+    with open(hparams_file) as fin:
+        hparams = load_hyperpyyaml(fin, overrides)
+
+    # Create experiment directory
+    sb.create_experiment_directory(
+        experiment_directory=hparams["output_folder"],
+        hyperparams_to_save=hparams_file,
+        overrides=overrides,
+    )
+
+    # Data preparation, to be run on only one process.
+    if not hparams["skip_prep"]:
+        sb.utils.distributed.run_on_main(
+            prepare_data,
+            kwargs={
+                "wav_folder": hparams["wav_folder"],
+                "audio_archive_path": hparams["audio_archive_path"],
+                "metadata_path": hparams["metadata_path"],
+                "manifest_train_path": hparams["train_annotation"],
+                "manifest_valid_path": hparams["valid_annotation"],
+                "manifest_test_path": hparams["test_annotation"],
+                "ratio": hparams["ratio"],
+                "random_seed": hparams["random_seed"]
+            },
+        )
+
+    # Create dataset objects
+    datasets = dataio_prep(hparams)
+
+    # Initialize the Brain object to prepare for mask training.
+    brain = DiagnosticsBrain(
+        modules=hparams["modules"],
+        opt_class=hparams["opt_class"],
+        hparams=hparams,
+        run_opts=run_opts,
+        checkpointer=hparams["checkpointer"],
+    )
+
+    # training loop
+    brain.fit(
+        epoch_counter=brain.hparams.epoch_counter,
+        train_set=datasets["train"],
+        valid_set=datasets["valid"],
+        train_loader_kwargs=hparams["dataloader_options"],
+        valid_loader_kwargs=hparams["dataloader_options"],
+    )
+
+    # Load the best checkpoint for evaluation
+    test_stats = brain.evaluate(
+        test_set=datasets["test"],
+        # min_key="error",
+        test_loader_kwargs=hparams["dataloader_options"],
+    )

30c1f522fee995f38ed6af136986e4173e7857b4
yizhu <Yi.Zhu@inrs.ca>
2023-04-28 15:50:20 -0700

    add wavlm to ssl pipeline

--- model/ssl_pipeline.py
+++ model/ssl_pipeline.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from speechbrain.lobes.models.huggingface_wav2vec import HuggingFaceWav2Vec2
 from speechbrain.lobes.models.huggingface_whisper import HuggingFaceWhisper
+from transformers import AutoModel, AutoProcessor
+from speechbrain.lobes.models.ECAPA_TDNN import AttentiveStatisticsPooling
 
 # model_hub_w2v2 = "facebook/wav2vec2-base-960h"
 # model_hub_whisper = "openai/whisper-tiny"
@@ -55,4 +57,73 @@ class SSL_diagnoser(nn.Module):
         ssl_output = self.pooling(ssl_output).squeeze(-1) # pool over time axis -> (batch, features)
         final_output = self.fc(ssl_output) # (batch, num_classes)
         return final_output
-        
+
+
+
+class SSL_diagnoser_v2(nn.Module):
+    def __init__(
+        self,
+        encoder: str = "microsoft/wavlm-base-plus",
+        pooling: str = "avg",
+        num_features: int = 768,
+        num_classes: int = 7,
+        freeze_encoder: bool = True,
+        *args,
+        **kwargs
+    ) -> None:
+        super().__init__(*args, **kwargs)
+        self.feature_extractor = AutoModel.from_pretrained(encoder)
+        for param in self.feature_extractor.parameters():
+            param.requires_grad = not freeze_encoder
+
+        self.fc = nn.Sequential(
+            nn.Linear(num_features, num_features),
+            nn.Dropout(p=0.25),
+            nn.LeakyReLU(0.1),
+            nn.Linear(num_features, num_classes)
+        )
+
+        self.weights_stack = nn.Parameter(torch.ones(self.feature_extractor.config.num_hidden_layers))
+        try:
+            self.processor = AutoProcessor.from_pretrained(encoder)
+        except:
+            self.processor = AutoProcessor.from_pretrained("facebook/wav2vec2-base-960h")
+
+        if pooling == "avg":
+            self.pooling = lambda x: F.adaptive_avg_pool1d(x, 1)
+        else:
+            self.pooling = AttentiveStatisticsPooling(
+                num_features,
+                attention_channels=num_features,
+                global_context=True
+            )
+
+    def forward(self, x):
+        # Preprocessing the data
+        input_values = self.processor(x, sampling_rate=16000, return_tensors="pt").input_values[0]
+        input_values = input_values.to(device=x.device, dtype=x.dtype)
+
+        # Extract wav2vec2 hidden states and perform a weighted sum
+        features = self.feature_extractor(input_values, output_hidden_states=True)
+        features = self.weighted_sum(features.hidden_states[1:])
+
+        # Pooling (on time dimension)
+        features = features.permute(0, 2, 1) # (batch, features, time) => (batch, time, features)
+        features = self.pooling(features).squeeze(-1)
+        output = self.fc(features)
+
+        return output
+
+    def weighted_sum(self, features):
+        layer_num = len(features)
+
+        # Perform the weighted sum
+        stacked_feature = torch.stack(features, dim=0)
+
+        _, *origin_shape = stacked_feature.shape
+        stacked_feature = stacked_feature.view(layer_num, -1)
+        norm_weights = F.softmax(self.weights_stack, dim=-1)
+        weighted_feature = (norm_weights.unsqueeze(-1) * stacked_feature).sum(dim=0)
+        weighted_feature = weighted_feature.view(*origin_shape)
+
+        return weighted_feature
